72ab39f0204a865e9ed9cb84d5d30c794ec9b160
AdrienLMR <adrienlemaire.pro32@gmail.com>
2024-01-28 16:19:02 +0100

    Add polaroïd scenes

--- Assets/Scripts/Anim/OnAnimLoad.cs
+++ Assets/Scripts/Anim/OnAnimLoad.cs
@@ -19,7 +19,7 @@ public class OnAnimLoad : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Loading.StartAnim();
+        Loading.Loaded();
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
--- Assets/Scripts/Anim/OnAnimPolaroïdEnd.cs
+++ Assets/Scripts/Anim/OnAnimPolaroïdEnd.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnAnimStart : StateMachineBehaviour
+public class OnAnimPolaroïdEnd : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -19,7 +19,7 @@ public class OnAnimStart : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Loading.Stop();
+        Polaroïd.AnimationEnded();
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
